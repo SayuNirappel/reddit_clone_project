403f2e5c4eea2e78ef02ed7d8eb92cb2a3f0e848
SayuNirappel <sayunirappel@gmail.com>
2025-03-13 11:04:57 +0530

    bottom navigation bar initiated

---
+++ lib/view/bottom_navigation_bar/bottom_navigation.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int slectedIndex = 0;
+  List screens = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
